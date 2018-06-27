4249907207defcdec700b2a76c068726765237a7
Andrey Drozdov <andrey.drozdov@nordigy.ru>
2018-06-27 19:38:21 +0300

    Logging steps implemented in abstract logger client

--- packages/testring-test-worker/src/worker/index.ts
+++ packages/testring-test-worker/src/worker/index.ts
@@ -1,8 +1,6 @@
-import { loggerClient } from '@testring/logger';
 import { transport } from '@testring/transport';
 import { WorkerController } from './worker-controller';
 
 const controller = new WorkerController(transport);
 
-loggerClient.setLogNestingLevel(3);
 controller.init();
