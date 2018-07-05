4dfac6d5436d01742f213efb883a8b60537f2633
Andrey Drozdov <andrey.drozdov@nordigy.ru>
2018-07-05 19:46:32 +0300

    Package initiated with basic webpack build config

---
+++ packages/testring-sample-app/webpack.config.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    }
+};
+
+export default config;
