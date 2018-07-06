586f9af41f71a7ca695677847ee64a8d732f39e5
Andrey Drozdov <andrey.drozdov@nordigy.ru>
2018-07-06 15:18:39 +0300

    recorder-app builds in production mode

--- packages/testring-recorder-app/webpack.config.ts
+++ packages/testring-recorder-app/webpack.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
+    mode: 'production',
     entry: './src/index.ts',
     module: {
         rules: [
