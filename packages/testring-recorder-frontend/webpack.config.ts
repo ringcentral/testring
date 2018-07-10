8c4eaddaab57add2d18c7d5553c20da5b8ab0510
luckyRoGGeR <warlack@outlook.com>
2018-07-10 18:53:35 +0300

    monaco does not working

--- packages/testring-recorder-frontend/webpack.config.ts
+++ packages/testring-recorder-frontend/webpack.config.ts
@@ -1,24 +1,44 @@
 import * as path from 'path';
 import * as webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 const config: webpack.Configuration = {
     mode: 'production',
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'stylesheets/[name].css'
+        }),
+
+        new MonacoWebpackPlugin({
+            languages: ['javascript']
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
             }
         ]
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
+        extensions: [ '.tsx', '.ts', '.js', '.css' ]
     }
 };
 
