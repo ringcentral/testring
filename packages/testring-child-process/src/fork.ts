69e5a7435d1fc6f0d2949d1c69b623f697a3c395
Elena Shenterova <elena.shenterova@nordigy.ru>
2018-07-11 17:05:14 +0300

    tests fix

--- packages/testring-child-process/src/fork.ts
+++ packages/testring-child-process/src/fork.ts
@@ -1,30 +1,15 @@
-import * as path from 'path';
 import * as childProcess from 'child_process';
 import { spawn } from './spawn';
 
 const EMPTY_PARAMETERS = [];
 const REQUIRE_TS_NODE = ['-r', 'ts-node/register'];
 
-const getAdditionalParameters = (filePath: string): Array<string> => {
-    const extension = path.extname(filePath);
-
-    switch (extension) {
-        case '.js':
-            return EMPTY_PARAMETERS;
-
-        case '.ts':
-            return REQUIRE_TS_NODE;
-
-        case '':
-            return require.extensions['.ts'] ?
-                REQUIRE_TS_NODE :
-                EMPTY_PARAMETERS;
-
-        default:
-            return EMPTY_PARAMETERS;
-    }
+const getAdditionalParameters = (): Array<string> => {
+    return require.extensions['.ts'] ?
+        REQUIRE_TS_NODE :
+        EMPTY_PARAMETERS;
 };
 
 export const fork = (filePath: string, args: Array<string> = []): childProcess.ChildProcess => {
-    return spawn('node', [...getAdditionalParameters(filePath), filePath, ...args]);
+    return spawn('node', [...getAdditionalParameters(), filePath, ...args]);
 };
