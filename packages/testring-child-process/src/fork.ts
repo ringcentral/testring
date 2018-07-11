225c85400198fd55c46b2ac2886451ecab6c5f30
johnthecat <zhuravlev1337@gmail.com>
2018-07-11 17:32:45 +0300

    fixed fork child in coverage task

--- packages/testring-child-process/src/fork.ts
+++ packages/testring-child-process/src/fork.ts
@@ -1,15 +1,57 @@
+import * as path from 'path';
+import * as process from 'process';
 import * as childProcess from 'child_process';
+import { resolveBinary } from './resolve-binary';
 import { spawn } from './spawn';
 
+const IS_WIN = process.platform === 'win32';
 const EMPTY_PARAMETERS = [];
 const REQUIRE_TS_NODE = ['-r', 'ts-node/register'];
 
-const getAdditionalParameters = (): Array<string> => {
-    return require.extensions['.ts'] ?
-        REQUIRE_TS_NODE :
-        EMPTY_PARAMETERS;
+const getAdditionalParameters = (filePath: string): Array<string> => {
+    const extension = path.extname(filePath);
+
+    switch (extension) {
+        case '.js':
+            return EMPTY_PARAMETERS;
+
+        case '.ts':
+            return REQUIRE_TS_NODE;
+
+        case '':
+            return require.extensions['.ts'] ?
+                REQUIRE_TS_NODE :
+                EMPTY_PARAMETERS;
+
+        default:
+            return EMPTY_PARAMETERS;
+    }
+};
+
+const getExecutor = (filePath: string): string => {
+    const extension = path.extname(filePath);
+
+    switch (extension) {
+        case '.js':
+            return 'node';
+
+        case '.ts':
+            return resolveBinary('ts-node');
+
+        case '':
+            return require.extensions['.ts'] ?
+                resolveBinary('ts-node') :
+                'node';
+
+        default:
+            return 'node';
+    }
 };
 
 export const fork = (filePath: string, args: Array<string> = []): childProcess.ChildProcess => {
-    return spawn('node', [...getAdditionalParameters(), filePath, ...args]);
+    if (IS_WIN) {
+        return spawn('node', [...getAdditionalParameters(filePath), filePath, ...args]);
+    } else {
+        return spawn(getExecutor(filePath), [filePath, ...args]);
+    }
 };
