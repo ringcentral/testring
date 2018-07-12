05bf7de8476f3bcd5ceeea982afe07f06da616e9
johnthecat <zhuravlev1337@gmail.com>
2018-07-12 14:01:36 +0300

    Added lerna-like publishing

--- bin/publish.js
+++ bin/publish.js
@@ -1,20 +1,60 @@
 const childProcess = require('child_process');
 const path = require('path');
 const glob = require('glob');
+const PackageUtilities = require('lerna/lib/PackageUtilities');
 const lernaConfig = require('../lerna.json');
 
-const results = [].concat(
+// FIXME temporary workaround, waiting for lerna 3 with separated version and publish commands
+// https://github.com/lerna/lerna/issues/961
+
+const packagesList = [].concat(
     ...lernaConfig.packages.map((pkg) => glob.sync(pkg))
 );
 
-results.forEach((pkg) => {
-    childProcess.execSync(
-        path.resolve('./node_modules/.bin/ci-publish'),
-        {
-            cwd: path.resolve(pkg),
-            stdio: process.stdio
+const packagesDescriptors = packagesList
+    .reduce((map, pkgPath) => {
+        const descriptor = require(path.resolve(pkgPath, 'package.json'));
+
+        if (!descriptor.private) {
+            map[pkgPath] = descriptor;
         }
-    );
+
+        return map;
+    }, {});
+
+const packagePaths = Object.keys(packagesDescriptors)
+    .reduce((map, pkgPath) => {
+        map[packagesDescriptors[pkgPath].name] = pkgPath;
+
+        return map;
+    }, {});
+
+const batchedPackages = PackageUtilities.topologicallyBatchPackages(Object.values(packagesDescriptors), {
+    depsOnly: true
 });
 
-process.stdout.write(`\nPackages published: ${results.length}\n`);
+PackageUtilities.runParallelBatches(batchedPackages, (pkg) => (callback) => {
+    process.stdout.write(`Publishing package: ${pkg.name}...\n`);
+
+    childProcess.exec(
+        path.resolve('./node_modules/.bin/ci-publish'),
+        {
+            cwd: path.resolve(packagePaths[pkg.name]),
+            stdio: process.stdio
+        },
+        (error) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback();
+            }
+        }
+    );
+}, 2, (error) => {
+    if (error) {
+        process.stderr.write(error.toString());
+        process.exit(1);
+    } else {
+        process.stdout.write(`\nPackages published: ${Object.keys(packagesDescriptors).length}\n`);
+    }
+});
