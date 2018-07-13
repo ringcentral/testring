794562396ba40078de424ddfbf10108b9d9e19c5
luckyRoGGeR <warlack@outlook.com>
2018-07-13 17:16:20 +0300

    traverse ready

---
+++ packages/testring-recorder-backend/src/get-cursor.ts
@@ -0,0 +1,67 @@
+import * as babylon from 'babylon';
+import traverse from 'babel-traverse';
+import * as t from 'babel-types';
+
+
+interface CursorPosition {
+    line: number;
+    column: number;
+}
+
+interface NodeLocation {
+    start: {
+        line: number;
+        column: number;
+    };
+    end: {
+        line: number;
+        column: number;
+    };
+}
+
+export function getCursor(code: string, currentPosition: CursorPosition): CursorPosition | null {
+
+    const ast = babylon.parse(code);
+    let wasCursorFound: any;
+
+    traverse(ast, {
+        enter(path) {
+            if (isCursorHere(path.node.loc, currentPosition)) {
+                wasCursorFound = path;
+            }
+        }
+    });
+
+    if (wasCursorFound) {
+        if (t.isArrowFunctionExpression(wasCursorFound.node)) {
+            const arrowExpression = wasCursorFound.findParent((parentPath) => t.isExpressionStatement(parentPath.node));
+
+            return arrowExpression.node.loc.end;
+        }
+
+        const externalNode = wasCursorFound.findParent((parentPath) => (
+            t.isBlockStatement(parentPath.parent) || t.isProgram(parentPath.parent)
+        ));
+
+        return externalNode.node.loc.end;
+    }
+
+    return null;
+
+}
+
+function isCursorHere(nodeLocation: NodeLocation, cursorLocation: CursorPosition) {
+
+    const isThatLine = (
+        nodeLocation.start.line <= cursorLocation.line &&
+        nodeLocation.end.line >= cursorLocation.line
+    );
+
+
+    const isThatColumn = (
+        nodeLocation.start.column <= cursorLocation.column &&
+        nodeLocation.end.column >= cursorLocation.column
+    );
+
+    return isThatLine && isThatColumn;
+}
---
+++ packages/testring-recorder-backend/test/get-cursor.spec.ts
@@ -0,0 +1,9 @@
+/// <reference types="mocha" />
+
+import * as chai from 'chai';
+
+describe('get-cursor', () => {
+    it('should get available position for cursor according to the current position', () => {
+
+    });
+});
