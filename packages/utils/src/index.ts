e9214b6836e5bfd0b578f011da01b74afce1a1d8
Serge Pavlyuk <flopss@gmail.com>
2018-08-22 18:01:57 +0300

    Added find-available-ports in utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,13 @@
-export { requirePackage, resolvePackage } from './package-require';
+export {
+    isAvailablePort,
+    getAvailablePort,
+    getRandomPort,
+    getAvailableFollowingPort,
+} from './find-available-ports';
+export {
+    requirePackage,
+    resolvePackage,
+} from './package-require';
 export { requirePlugin } from './plugin-require';
 export { Queue } from './queue';
 export { Stack } from './stack';
