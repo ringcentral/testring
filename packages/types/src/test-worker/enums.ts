8b464ec3e7edc42b944c70d3d6a17a6b597d8fef
Serge Pavlyuk <flopss@gmail.com>
2018-09-07 07:14:44 +0300

    beforeCompile hook for TestWorker

--- packages/types/src/test-worker/enums.ts
+++ packages/types/src/test-worker/enums.ts
@@ -1,20 +1,21 @@
 export const enum TestWorkerPlugin {
-    compile = 'compile'
+    beforeCompile = 'beforeCompile',
+    compile = 'compile',
 }
 
 export const enum TestEvents {
     started = 'test/started',
     finished = 'test/finished',
-    failed = 'test/failed'
+    failed = 'test/failed',
 }
 
 export const enum TestStatus {
     idle = 'idle',
     done = 'done',
-    failed = 'failed'
+    failed = 'failed',
 }
 
 export const enum TestWorkerAction {
     executeTest = 'executeTest',
-    executionComplete = 'executionComplete'
+    executionComplete = 'executionComplete',
 }
