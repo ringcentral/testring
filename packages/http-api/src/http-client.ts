3569cbd65dfc52833469039c7b68f89e5cb754f1
Serge Pavlyuk <flopss@gmail.com>
2018-11-21 16:38:56 +0300

    Isomorphic http client

--- packages/http-api/src/http-client.ts
+++ packages/http-api/src/http-client.ts
@@ -1,11 +1,20 @@
+import { isChildProcess } from '@testring/child-process';
 import { loggerClient } from '@testring/logger';
 import { IHttpRequestMessage, HttpMessageType } from '@testring/types';
 import { AbstractHttpClient } from './abstract-http-client';
 
+const logger = loggerClient.withPrefix('[http client]');
+
 export class HttpClient extends AbstractHttpClient {
     protected broadcast(request: IHttpRequestMessage) {
-        loggerClient.verbose('[http client] send message to parent process', request);
+        if (isChildProcess()) {
+            logger.verbose('send message to parent process', request);
 
-        this.transportInstance.broadcast(HttpMessageType.send, request);
+            this.transportInstance.broadcast(HttpMessageType.send, request);
+        } else {
+            logger.verbose('send message inside root process', request);
+
+            this.transportInstance.broadcastLocal(HttpMessageType.send, request);
+        }
     }
 }
