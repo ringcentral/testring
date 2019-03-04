733f6daf41f81525cb8e90e50be5fd90728f8801
ledamint <heresliker@gmail.com>
2019-03-04 17:18:54 +0300

    WAT-537 create TestManager

---
+++ packages/recorder-backend/src/test-manager.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import { TestWriter } from './test-writer';
+import { actionsByRecordingEventTypes } from '@testring/types';
+
+type TestManagerOptions = {
+    manager: string;
+};
+
+export class TestManager {
+    manager: string;
+    testWriter: TestWriter;
+
+    constructor({ manager }: TestManagerOptions) {
+        this.manager = manager;
+
+        this.testWriter = new TestWriter(path.resolve(__dirname, 'test.js'));
+    }
+
+    initialize() {
+        this.testWriter.addLine(`const ${this.manager} = await this.get${this.manager};`);
+    }
+
+    getActionLine(action: string, path: string) {
+        return `${this.manager}.${action}(${path});`;
+    }
+
+    handleAction(actionInfo) {
+        const path = 'root.abc.abc';
+        const action = actionsByRecordingEventTypes[actionInfo.type];
+        const line = this.getActionLine(action, path);
+
+        this.testWriter.addLine(line);
+    }
+
+    finishTest() {
+        this.testWriter.write();
+    }
+}
