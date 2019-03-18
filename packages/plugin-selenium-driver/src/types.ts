035d2c08b92231fc9cc91d99da9d0b1fa1545d94
Serge Pavlyuk <flopss@gmail.com>
2019-03-18 16:26:56 +0300

    Recorder extension as argument

--- packages/plugin-selenium-driver/src/types.ts
+++ packages/plugin-selenium-driver/src/types.ts
@@ -1,6 +1,7 @@
 import { Config } from 'webdriverio';
 
 export type SeleniumPluginConfig = Config & {
+    recorderExtension: boolean;
     clientCheckInterval: number;
     clientTimeout: number;
 };
