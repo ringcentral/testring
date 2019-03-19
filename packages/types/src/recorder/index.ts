5634459e74343ed22460b3c7b7c3ceff7c612d4e
ledamint <heresliker@gmail.com>
2019-03-19 12:46:29 +0300

    WAT-537 add BrowserEventMap

--- packages/types/src/recorder/index.ts
+++ packages/types/src/recorder/index.ts
@@ -34,5 +34,6 @@ export type BrowserEventInfo = {
     type: RecordingEventTypes;
     affectedElementsSummary: ElementSummary[];
     domSummary: ElementSummary;
+    id: string;
     path?: string;
 };
