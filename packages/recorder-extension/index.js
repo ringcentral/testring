8d5f780d5bd41d64ad6db04388d9ffc26f622a4a
Serge Pavlyuk <flopss@gmail.com>
2019-04-15 01:16:04 +0300

    Fixed initial build

--- packages/recorder-extension/index.js
+++ packages/recorder-extension/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const reportFile = path.join(__dirname, 'extension/testring-dev.crx');
+const crxFile = path.join(__dirname, 'extension/testring-dev.crx');
+const reportFile = path.join(__dirname, 'extension/testring-dev.json');
 
-if (!fs.existsSync(reportFile)) {
+if (!fs.existsSync(crxFile)) {
     throw Error('Please rebuild extension needed files not found');
 }
 
