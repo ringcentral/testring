3882ae7a2ed4cd901e0e134eb1d7fdfe58c09067
Serge Pavlyuk <flopss@gmail.com>
2019-05-20 22:26:11 +0300

    babel-plugin-transform-function-bind plugin support

--- packages/devtool-execution-wrapper/src/index.ts
+++ packages/devtool-execution-wrapper/src/index.ts
@@ -1,11 +1,14 @@
 import { transformAsync } from '@babel/core';
 import { devToolExecutionWrapper } from './babel-devtool-execution-wrapper';
 
+const babelPluginTransofrmFunctionBind = require('babel-plugin-transform-function-bind');
+
 export const devtoolExecutionWrapper = async (source: string, filename: string): Promise<string> => {
     const result = await transformAsync(source, {
         filename,
         sourceMaps: 'inline',
         plugins: [
+            babelPluginTransofrmFunctionBind,
             devToolExecutionWrapper,
         ],
     });
