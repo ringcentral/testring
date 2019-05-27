cfc47ca8b9664e1dd8df5851852111cc89284a41
Serge Pavlyuk <flopss@gmail.com>
2019-05-28 01:13:07 +0300

    Removed babel-plugin-transform-function-bind

--- packages/devtool-execution-plugin/src/index.ts
+++ packages/devtool-execution-plugin/src/index.ts
@@ -1,14 +1,11 @@
 import { transformAsync } from '@babel/core';
 import { devToolExecutionWrapper } from './babel-devtool-execution-wrapper';
 
-const babelPluginTransformFunctionBind = require('babel-plugin-transform-function-bind');
-
 export const devtoolExecutionWrapper = async (source: string, filename: string): Promise<string> => {
     const result = await transformAsync(source, {
         filename,
         sourceMaps: 'inline',
         plugins: [
-            babelPluginTransformFunctionBind,
             devToolExecutionWrapper,
         ],
     });
