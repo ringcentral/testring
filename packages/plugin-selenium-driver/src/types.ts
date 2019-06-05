2543d6b079c285c6f8998e4d8b7fa28322b2b019
Serge Pavlyuk <flopss@gmail.com>
2019-06-05 16:50:52 +0300

    Update chromedriver and ability to pass own chromeDriverPath

--- packages/plugin-selenium-driver/src/types.ts
+++ packages/plugin-selenium-driver/src/types.ts
@@ -1,6 +1,7 @@
 import { Config } from 'webdriverio';
 
 export type SeleniumPluginConfig = Config & {
+    chromeDriverPath?: string;
     recorderExtension: boolean;
     clientCheckInterval: number;
     clientTimeout: number;
