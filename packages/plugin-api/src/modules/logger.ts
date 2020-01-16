fb2b87bf8e9a043aeb6760b7f4e5d639d94bedea
Maxim Lebedev <maxim.lebedev@nordigy.ru>
2020-01-16 19:09:09 +0300

    Change signatures of LoggerPlugins hooks handlers

--- packages/plugin-api/src/modules/logger.ts
+++ packages/plugin-api/src/modules/logger.ts
@@ -1,17 +1,17 @@
 import { AbstractAPI } from './abstract';
-import { LoggerPlugins } from '@testring/types';
+import { ILogEntity, ILogMeta, LoggerPlugins } from '@testring/types';
 
 export class LoggerAPI extends AbstractAPI {
 
-    beforeLog(handler: (log) => void) {
+    beforeLog(handler: (logEntity: ILogEntity, meta: ILogMeta) => ILogEntity) {
         this.registryWritePlugin(LoggerPlugins.beforeLog, handler);
     }
 
-    onLog(handler: (log) => void) {
+    onLog(handler: (logEntity: ILogEntity, meta: ILogMeta) => void) {
         this.registryReadPlugin(LoggerPlugins.onLog, handler);
     }
 
-    onError(handler: (log) => void) {
+    onError(handler: (error: any, meta: ILogMeta) => void) {
         this.registryWritePlugin(LoggerPlugins.onError, handler);
     }
 }
