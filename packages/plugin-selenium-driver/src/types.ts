c1eae4ced9a35e199df7972d07975dbf7c37a3d8
Maxim Kavsadze <maksim.lv@mail.ru>
2020-03-23 22:35:43 +0300

    WAT-885: completely work

--- packages/plugin-selenium-driver/src/types.ts
+++ packages/plugin-selenium-driver/src/types.ts
@@ -5,4 +5,6 @@ export type SeleniumPluginConfig = WebdriverIO.RemoteOptions & Config & {
     recorderExtension: boolean;
     clientCheckInterval: number;
     clientTimeout: number;
+    host?: string; // fallback for configuration. In WebdriverIO 5 field host renamed to hostname
+    desiredCapabilities?: any; // fallback for configuration. In WebdriverIO 5 field renamed
 };
