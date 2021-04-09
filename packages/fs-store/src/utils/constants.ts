dd45cfdf3d39e07dfad2dea7ad1defe6b0b89392
Sergei.Zhurin <Sergei.Zhurin@nordigy.ru>
2021-04-09 17:36:01 +0300

    WAT-1070 comb code

--- packages/fs-store/src/utils/constants.ts
+++ packages/fs-store/src/utils/constants.ts
@@ -14,13 +14,6 @@ export const FS_CONSTANTS: Record<string, string> = {
   FAS_RELEASE_POSTFIX: '_release_thread',
   FAS_CLEAN_POSTFIX: '_release_worker_threads',
 
-  FS_COL_REQ_ST_POSTFIX: '_req_state',
-  FS_COL_RESP_ST_POSTFIX: '_state',
-  FS_COL_FILTER_REQ_POSTFIX: '_filter_list_req',
-  FS_COL_FILTER_RESP_POSTFIX: '_filter_list_resp',
-  FS_COL_CONFIRM_REQ_POSTFIX: '_list_confirm_req',
-  FS_COL_CONFIRM_RESP_POSTFIX: '_list_confirm_resp',
-
   FS_DEFAULT_MSG_PREFIX: 'fs-store',
   FS_DEFAULT_QUEUE_PREFIX: 'fs-que',
   FS_DEFAULT_COLLECTION_PREFIX: 'fs-col',
