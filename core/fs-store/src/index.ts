8b5fd7f7575ce1af2e57fabffba95f8390cef735
Sergei.Zhurin <Sergei.Zhurin@nordigy.ru>
2022-11-17 02:25:21 +0400

    WAT-1309 fixed events mem leak

--- core/fs-store/src/index.ts
+++ core/fs-store/src/index.ts
@@ -6,5 +6,7 @@ import {create as FSBinaryFactory} from './FSBinaryFileFactory';
 export {FSCoverageFactory, FSScreenshotFactory, FSTextFactory, FSBinaryFactory};
 export {FSStoreFile} from './fs-store-file';
 export {FSStoreClient} from './fs-store-client';
+export {default as FSClientGet} from './fs-store-client-fabric';
+
 export {FSStoreServer, fsStoreServerHooks} from './fs-store-server';
 export {FS_CONSTANTS} from './utils';
