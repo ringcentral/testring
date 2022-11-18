b181c6f23e37a4302db0426524a4263fffa41fee
artem.sukhinin <artem.sukhinin@ab-soft.net>
2022-11-18 13:01:54 +0100

    WAT-1309 add logs to debug

--- packages/plugin-selenium-driver/src/plugin/index.ts
+++ packages/plugin-selenium-driver/src/plugin/index.ts
@@ -85,6 +85,8 @@ export class SeleniumPlugin implements IBrowserProxyPlugin {
     constructor(config: Partial<SeleniumPluginConfig> = {}) {
         this.config = this.createConfig(config);
 
+        console.log(`SeleniumPluginConfig: ${JSON.stringify(this.config, null, 2)}`);
+
         if (this.config.host === undefined) {
             this.runLocalSelenium();
         }
