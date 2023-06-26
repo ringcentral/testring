f8db937df1c48b8681fb2a6b46c0a4a73ac0bef7
Sergei.Zhurin <Sergei.Zhurin@nordigy.ru>
2023-06-26 14:26:13 +0400

    WAT-1309 ensure work process

--- core/utils/src/plugin-require.ts
+++ core/utils/src/plugin-require.ts
@@ -20,16 +20,28 @@ function normalizeExport(module) {
 
 export function requirePlugin(pluginPath: string): any {
     let resolvedPlugin;
-    const parentModule = path.join(__dirname, '../..')
+    const parentModule = path.join(__dirname, '../..');
+
+    const echo = pluginPath.includes('store');
 
     for (let index = 0; index < PREFIXES.length; index++) {
         try {
-            resolvedPlugin = resolvePackage(PREFIXES[index] + pluginPath, parentModule);
+            resolvedPlugin = resolvePackage(
+                PREFIXES[index] + pluginPath,
+                parentModule,
+            );
         } catch (e) {
+            if (echo) {
+                // eslint-disable-next-line no-console
+                console.log('error', {path: PREFIXES[index] + pluginPath, parentModule});
+            }
             continue;
         }
         break;
     }
+
+    // eslint-disable-next-line no-console
+    console.log('res', {resolvedPlugin});
 
     if (!resolvedPlugin) {
         resolvedPlugin = resolvePackage(pluginPath);
