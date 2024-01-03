d717f27f1b9374742d157ee12a41ae9e4f57e7f5
ArtMathArt <sukhart.work27@gmail.com>
2024-01-03 11:17:30 +0100

    WAT-2874

--- core/fs-reader/src/file-locator.ts
+++ core/fs-reader/src/file-locator.ts
@@ -6,9 +6,7 @@ export function locateFiles(searchpath: string): Promise<string[]> {
             return resolve([]);
         }
 
-        let patternInternal = fg.convertPathToPattern(searchpath);
-        console.log('patternInternal', patternInternal);
-        fg.async(patternInternal).then((files) => {
+        fg.async(searchpath).then((files) => {
             resolve(files);
         }).catch((error) => {
             reject(error);
