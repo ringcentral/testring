4c88762321fb86ce6b28e17e34688649ce88719f
ArtMathArt <sukhart.work27@gmail.com>
2024-01-03 11:51:47 +0100

    WAT-2874

--- core/fs-reader/src/file-locator.ts
+++ core/fs-reader/src/file-locator.ts
@@ -1,12 +1,13 @@
 import * as fg from 'fast-glob';
 
 export function locateFiles(searchpath: string): Promise<string[]> {
+    const searchPattern = searchpath && process.platform === 'win32' ? fg.convertPathToPattern(searchpath) : searchpath;
     return new Promise((resolve, reject) => {
         if (!searchpath) {
             return resolve([]);
         }
 
-        fg.async(searchpath).then((files) => {
+        fg.async(searchPattern).then((files) => {
             resolve(files);
         }).catch((error) => {
             reject(error);
