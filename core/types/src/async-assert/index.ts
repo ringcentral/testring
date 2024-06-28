29ee91f389690bb1ed7897e54d75bd3a0231a5e3
Ilia Marchenko <ilia.marchenko@ringcentral.com>
2024-06-28 13:59:06 +0300

    WAT-3484

--- core/types/src/async-assert/index.ts
+++ core/types/src/async-assert/index.ts
@@ -1,3 +1,8 @@
+import type { use as chaiUse } from 'chai';
+
+type First<T> = T extends [infer A, ...any[]] ? A : never;
+type ChaiPlugin = First<Parameters<typeof chaiUse>>;
+
 export interface IAssertionSuccessMeta {
     isSoft: boolean;
     successMessage?: string;
@@ -15,4 +20,5 @@ export interface IAssertionOptions {
     isSoft?: boolean;
     onSuccess?: (IAssertionSuccessMeta) => void | Promise<void>;
     onError?: (IAssertionErrorMeta) => void | Error | Promise<void | Error>;
+    plugins?: ChaiPlugin[];
 }
