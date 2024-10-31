72d1d1de586e7cd920e87824dcbc005f6f211a10
ArtMathArt <sukhart.work27@gmail.com>
2024-10-31 17:43:00 +0200

    Change to parseSync in browser-proxy

--- packages/browser-proxy/src/browser-proxy/index.ts
+++ packages/browser-proxy/src/browser-proxy/index.ts
@@ -2,7 +2,7 @@ import * as yargs from 'yargs';
 import {transport} from '@testring/transport';
 import {BrowserProxy} from './browser-proxy';
 
-const args = yargs.argv;
+const args = yargs.parseSync();
 
 const name = args.name as string;
 const config = JSON.parse(args.config as string);
