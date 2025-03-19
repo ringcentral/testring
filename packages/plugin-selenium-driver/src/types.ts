a82e66e46fcec43c57763fd46db15423e9478f7b
ArtMathArt <sukhart.work27@gmail.com>
2025-03-19 16:42:12 +0200

    WAT-4512

--- packages/plugin-selenium-driver/src/types.ts
+++ packages/plugin-selenium-driver/src/types.ts
@@ -9,4 +9,5 @@ export type SeleniumPluginConfig = Capabilities.WebdriverIOConfig & {
     desiredCapabilities?: Capabilities.RequestedStandaloneCapabilities[]; // fallback for configuration. In WebdriverIO 5 field renamed
     cdpCoverage: boolean;
     workerLimit?: number | 'local';
+    disableClientPing?: boolean;
 };
